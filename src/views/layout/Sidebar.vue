a9b1586a7271b5cf40e1bb9aa5509da221c60aea
JhonXY <1076849402@qq.com>
2017-09-09 11:36:34 +0800

    permission

--- src/views/layout/Sidebar.vue
+++ src/views/layout/Sidebar.vue
@@ -1,11 +1,26 @@
 <template lang="html">
-
+  <el-menu mode="vertical" theme="dark" :default-active="$route.path">
+    <sidebar-item :routes='permission_routers'></sidebar-item>
+  </el-menu>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
 export default {
+  components: {
+    SidebarItem
+  },
+  computed: {
+    ...mapGetters([
+      'permission_routers'
+    ])
+  }
 }
 </script>
 
-<style lang="css">
+<style rel="stylesheet/scss" lang="scss" scoped>
+.el-menu {
+  min-height: 100%;
+}
 </style>
---
+++ src/views/table/index.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <div class="app-container">
+    <p>1</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
